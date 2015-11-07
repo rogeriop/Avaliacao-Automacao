be6c486f906c36e640f4bf51ff121438862d33cd
labcst2 <labcst2@UNI008-L002-011.serlab.intra>
2015-11-07 14:59:45 -0300

    Commit na UNINASSAU

--- src/manipulacao/NovaSimulacao.java
+++ src/manipulacao/NovaSimulacao.java
@@ -13,7 +13,7 @@ public class NovaSimulacao {
 	public void test() {
 		FirefoxDriver driver = new FirefoxDriver();
 		
-		WebDriverWait wait = new WebDriverWait(driver, 10);
+//		WebDriverWait wait = new WebDriverWait(driver, 10);
 		
 		driver.get("http://eliasnogueira.com/uninassau/financiamento/index.html");
 		driver.findElementById("residencial").click();
